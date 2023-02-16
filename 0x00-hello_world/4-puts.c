0a875c5f22a01c78e8feb000bf1169c5ea3258af
Itolondo <254sospetre@gmail.com>
2023-02-16 13:25:33 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Not allowed to use printf
+ *Return: Always 0 (Success)
+ *
+ */
+Int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+Return(0)
+}
