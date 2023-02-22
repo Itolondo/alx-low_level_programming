48d3684e99395fdd870d5d51079cfc36ed551149
Itolondo <254sospetre@gmail.com>
2023-02-21 18:05:25 -0800

    1-main.c

---
+++ 0x02-functions_nested_loops/_alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+void print _alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z': c++)
+	{
+		putchar(c);
+	}
+	_putchar(\n');
+}
