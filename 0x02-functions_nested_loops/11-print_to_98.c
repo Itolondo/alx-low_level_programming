6be7604eb43c16866adabdf1d3793001a637b0ac
Itolondo <254sospetre@gmail.com>
2023-02-22 18:22:16 -0800

    100-times_table

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,38 +9,17 @@
  */
 
 void print_to_98(int n)
-
 {
-	if (n <= 98)
+	while (n < 98)
 	{
-		for (; n <= 98; n++)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-			printf("\n");
-			break;
-		}
-		else
-		{
-			printf("%d, ", n);
-		}
+		printf("%i, ", n);
+		n++;
 	}
-	}
-else
-{
-	for (; n >= 98; n--)
+	while (n > 98)
 	{
-		if (n == 98)
-		{
-			printf("%d", n);
-			printf("\n");
-			break;
-		}
-		else
-		{
-			printf("%d, ", n);
-		}
+		printf("%i, ", n);
+		n--;
 	}
-}
+	printf("98");
+	putchar('\n');
 }
