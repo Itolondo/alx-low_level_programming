8561ff159129d39a97071ed51781edecb18041d9
Itolondo <254sospetre@gmail.com>
2023-02-26 22:01:43 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * reset_to_98 - Function taking a pointer as parameter updating the calue to 98
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
